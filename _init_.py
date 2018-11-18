20324e8f4a69f0cf87e24722d33305bd59a50bdc
ashish <ashishunhale.1420@gmail.com>
2018-11-18 15:17:28 +0530

    Order Types and other entries

--- _init_.py
+++ _init_.py
@@ -1,9 +1,9 @@
 import JindalStel as JStel
-import login as loginDetails
+import login
 
 #The login to be implemented for the Upstox apito coonect to the market
 # cureently contains the backtesting on local data
 if __name__ == '__main__':
-    loginDetails.login()
+    login.loginInit.__login__()
     JStel.jindalStel()
 
